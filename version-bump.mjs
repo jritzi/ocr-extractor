5e20e098902bcb15ae4ca81307aaa087d765cd67
Johnathan Ritzi <jrdioko@gmail.com>
2026-04-23 20:27:24 -0600

    Fix typo

--- version-bump.mjs
+++ version-bump.mjs
@@ -7,7 +7,7 @@ const targetVersion = process.env.npm_package_version;
 let manifest = JSON.parse(readFileSync("manifest.json", "utf8"));
 const { minAppVersion } = manifest;
 manifest.version = targetVersion;
-writeFileSync("anifest.json", JSON.stringify(manifest, null, "\t"));
+writeFileSync("manifest.json", JSON.stringify(manifest, null, "\t"));
 
 // update versions.json with target version and minAppVersion from manifest.json
 let versions = JSON.parse(readFileSync("versions.json", "utf8"));
