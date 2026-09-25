f69ee7d9ff2bec7500be751b0d9862322d36d0e2
Johnathan Ritzi <jrdioko@gmail.com>
2026-09-25 17:14:58 -0600

    Use ConfirmationModal

--- src/ui/confirm-extract-all-modal.ts
+++ src/ui/confirm-extract-all-modal.ts
@@ -1,25 +1,19 @@
-import { App, Modal, Setting } from "obsidian";
+import { App, ConfirmationModal } from "obsidian";
 import { t } from "../i18n";
 
-export class ConfirmExtractAllModal extends Modal {
+export class ConfirmExtractAllModal extends ConfirmationModal {
   constructor(app: App, onSubmit: () => void) {
     super(app);
     this.setTitle(t("modals.extractAll.title"));
 
     this.contentEl.createEl("p", { text: t("modals.extractAll.warning") });
 
-    new Setting(this.contentEl)
-      .addButton((btn) =>
-        btn.setButtonText(t("common.cancel")).onClick(() => this.close()),
-      )
-      .addButton((btn) =>
-        btn
-          .setButtonText(t("modals.extractAll.extract"))
-          .setCta()
-          .onClick(() => {
-            this.close();
-            onSubmit();
-          }),
-      );
+    this.addButton((button) =>
+      button
+        .setButtonText(t("modals.extractAll.extract"))
+        .setCta()
+        .onClick(onSubmit),
+    );
+    this.addCancelButton(t("common.cancel"));
   }
 }
