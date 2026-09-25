88353452a229f418811e9c8f938ef766df67370a
Johnathan Ritzi <jrdioko@gmail.com>
2026-09-25 17:17:58 -0600

    Use ConfirmationModal

--- src/ui/installer-update-modal.ts
+++ src/ui/installer-update-modal.ts
@@ -1,31 +1,33 @@
-import { App, Modal, Setting } from "obsidian";
+import { App, ConfirmationModal } from "obsidian";
 import { t } from "../i18n";
 
-export class InstallerUpdateModal extends Modal {
+const KEEP_OPEN = true;
+
+export class InstallerUpdateModal extends ConfirmationModal {
   constructor(app: App) {
     super(app);
     this.setTitle(t("modals.installerUpdate.title"));
-  }
 
-  onOpen() {
     this.contentEl.createEl("p", { text: t("modals.installerUpdate.body") });
 
-    new Setting(this.contentEl)
-      .addButton((btn) =>
-        btn.setButtonText(t("common.dismiss")).onClick(() => this.close()),
-      )
-      .addButton((btn) =>
-        btn
-          .setButtonText(t("modals.installerUpdate.learnMore"))
-          .onClick(() =>
-            window.open("https://obsidian.md/help/updates#Installer+updates"),
-          ),
-      )
-      .addButton((btn) =>
-        btn
-          .setButtonText(t("modals.installerUpdate.download"))
-          .setCta()
-          .onClick(() => window.open("https://obsidian.md/download")),
-      );
+    this.addButton((button) =>
+      button
+        .setButtonText(t("modals.installerUpdate.learnMore"))
+        .setSecondary()
+        .onClick(() => {
+          window.open("https://obsidian.md/help/updates#Installer+updates");
+          return KEEP_OPEN;
+        }),
+    );
+    this.addButton((button) =>
+      button
+        .setButtonText(t("modals.installerUpdate.download"))
+        .setCta()
+        .onClick(() => {
+          window.open("https://obsidian.md/download");
+          return KEEP_OPEN;
+        }),
+    );
+    this.addCancelButton(t("common.dismiss"));
   }
 }
